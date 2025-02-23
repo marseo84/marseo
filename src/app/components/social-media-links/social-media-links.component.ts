446b91f089b1eae519e6d4b990e5d73613eeec09
Marcel Bosek <himarseo@gmail.com>
2025-02-23 12:59:41 +0100

    social links

--- src/app/components/social-media-links/social-media-links.component.ts
+++ src/app/components/social-media-links/social-media-links.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-social-media-links',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './social-media-links.component.html',
   styleUrl: './social-media-links.component.scss',
   standalone: true,
 })
-export class SocialMediaLinksComponent {}
+export class SocialMediaLinksComponent {
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faInstagram = faInstagram;
+}
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,10 +3,17 @@ import { HeroComponent } from '../../components/hero/hero.component';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from '../../components/skills/skills.component';
+import { SocialMediaLinksComponent } from '../../components/social-media-links/social-media-links.component';
 
 @Component({
   selector: 'app-about',
-  imports: [HeroComponent, CommonModule, RouterModule, SkillsComponent],
+  imports: [
+    HeroComponent,
+    CommonModule,
+    RouterModule,
+    SkillsComponent,
+    SocialMediaLinksComponent,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   standalone: true,
